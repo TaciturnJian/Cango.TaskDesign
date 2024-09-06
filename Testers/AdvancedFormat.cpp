290b9ff4de0c78e212f6e7ebc1cedeed9bcb8498
Sango <letusgoone@outlook.com>
2024-09-06 09:23:38 +0800

    使用 std::format

--- Testers/AdvancedFormat.cpp
+++ Testers/AdvancedFormat.cpp
@@ -1,49 +1,31 @@
+#include <algorithm>
+#include <format>
+#include <iomanip>
 #include <iostream>
-#include <vector>
-#include <spdlog/spdlog.h>
-#include <Cango/CommonUtils/FormatExtension.hpp>
+#include <sstream>
+#include <string_view>
 
+struct TestObject2 {
+	int V3;
+	float V2;
+	double V1;
+};
 
-namespace {
-	struct TestObject {
-		int V1;
-		float V2;
-	};
+template <>
+struct std::formatter<TestObject2, char> {
+	constexpr auto parse(std::format_parse_context& context) {
+		const auto it = context.begin();
+		if (*it != '}') throw std::format_error("invalid format");
+		return it;
+	}
 
-	struct TestObject2 {
-		double V1;
-		float V2;
-		int V3;
-	};
-}
-
-namespace fmt {
-	// {:1} -> V1
-	// {:2} -> V2
-	// {} -> V1, V2
-	template <>
-	struct formatter<TestObject> : char_status_parser {
-		auto format(const TestObject& obj, auto& context) const {
-			switch (Status) {
-			case 'a':
-				return format_to(context.out(), "{}", obj.V1);
-			case 'b':
-				return format_to(context.out(), "{}", obj.V2);
-			default:
-				return format_to(context.out(), "{},{}", obj.V1, obj.V2);
-			}
-		}
-	};
-
-	template <>
-	struct formatter<TestObject2> : empty_parser {
-		auto format(const TestObject2& obj, auto& context) {
-			return format_to(context.out(), "{},{},{}", obj.V1, obj.V2, obj.V3);
-		}
-	};
-}
+	auto format(const TestObject2& obj, std::format_context& context) const {
+		return std::format_to(context.out(), "{} {} {}", obj.V1, obj.V2, obj.V3);
+	}
+};
 
 int main() {
-	const TestObject object{1, 2};
-	fmt::print("[[{0}]] {1} {2}", fmt::join(std::vector{object, object}, "], ["), TestObject2{});
+	TestObject2 object2{};
+	std::cout << std::format("{} {}\n", 1, object2);
+	return 0;
 }
